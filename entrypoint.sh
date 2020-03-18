d5303fbfefa6a7877644a8105cabcb3e30f8cd53
Adam Grzybkowski <AdamGrzybkowski@users.noreply.github.com>
2020-03-18 13:13:03 +0100

    Release notes git generation

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,3 +1,18 @@
 #!/bin/sh
-appcenter distribute release --token "$INPUT_TOKEN" --app "$INPUT_APPNAME" --group "$INPUT_GROUP" --file "$INPUT_FILE" --release-notes "$INPUT_RELEASENOTES" --debug
+RELEASE_NOTES=""
 
+if [ -n "${INPUT_RELEASENOTES}" ]; then
+    RELEASE_NOTES=${INPUT_RELEASENOTES}
+elif [ $INPUT_GITRELEASENOTES ]; then
+    RELEASE_NOTES="$(git log -1 --pretty=short)"
+fi
+
+appcenter \
+        distribute \
+        release \
+        --token "$INPUT_TOKEN" \
+        --app "$INPUT_APPNAME" \
+        --group "$INPUT_GROUP" \
+        --file "$INPUT_FILE" \
+        --release-notes "${RELEASE_NOTES}" \
+        --debug
