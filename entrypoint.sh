1e6f777729a28635f48544085ac704171531c860
Tony Morone <tony.morone@resideo.com>
2020-07-07 12:06:08 -0500

    Add notifyTesters option that will remove the --silent flag

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,6 +3,8 @@ RELEASE_NOTES=""
 isFirst=true
 releaseId=""
 export IFS=";"
+params=()
+[ "${INPUT_NOTIFYTESTERS}" != true ] && params+=(--silent)
 if [ -n "${INPUT_RELEASENOTES}" ]; then
     RELEASE_NOTES=${INPUT_RELEASENOTES}
 elif [ $INPUT_GITRELEASENOTES ]; then
@@ -11,9 +13,9 @@ fi
 for group in $INPUT_GROUP; do
     if ${isFirst} ; then
         isFirst=false
-        appcenter distribute release --token "$INPUT_TOKEN" --app "$INPUT_APPNAME" --group $group --file "$INPUT_FILE" --release-notes "$INPUT_RELEASENOTES" --silent
+        appcenter distribute release --token "$INPUT_TOKEN" --app "$INPUT_APPNAME" --group $group --file "$INPUT_FILE" --release-notes "$INPUT_RELEASENOTES" "${params[@]}"
         releaseId=$(appcenter distribute releases list --token "$INPUT_TOKEN"  --app "$INPUT_APPNAME" | grep ID | head -1 | tr -s ' ' | cut -f2 -d ' ')
     else
-        appcenter distribute releases add-destination --token "$INPUT_TOKEN" -d $group -t group -r $releaseId -s --app "$INPUT_APPNAME"
+        appcenter distribute releases add-destination --token "$INPUT_TOKEN" -d $group -t group -r $releaseId --app "$INPUT_APPNAME" "${params[@]}"
     fi
 done
