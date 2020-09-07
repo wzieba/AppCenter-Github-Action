41b8c2d7690c52b57e5559a4bece277544217f05
Mateusz Kwieciński <36954793+mateuszkwiecinski@users.noreply.github.com>
2020-09-07 12:19:04 +0200

    Fix gitReleaseNotes not being handled properly (#25)

--- entrypoint.sh
+++ entrypoint.sh
@@ -14,7 +14,7 @@ fi
 for group in $INPUT_GROUP; do
     if ${isFirst} ; then
         isFirst=false
-        appcenter distribute release --token "$INPUT_TOKEN" --app "$INPUT_APPNAME" --group $group --file "$INPUT_FILE" --release-notes "$INPUT_RELEASENOTES" "${params[@]}"
+        appcenter distribute release --token "$INPUT_TOKEN" --app "$INPUT_APPNAME" --group $group --file "$INPUT_FILE" --release-notes "$RELEASE_NOTES" "${params[@]}"
         releaseId=$(appcenter distribute releases list --token "$INPUT_TOKEN"  --app "$INPUT_APPNAME" | grep ID | tr -s ' ' | cut -f2 -d ' ' | sort -n -r | head -1)
     else
         appcenter distribute releases add-destination --token "$INPUT_TOKEN" -d $group -t group -r $releaseId --app "$INPUT_APPNAME" "${params[@]}"
